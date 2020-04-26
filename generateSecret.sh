404093aad6558601c6ad5d9dc8174b1ab5c0e524
Ningfeng <1733116199@qq.com>
2020-04-26 15:45:43 -0700

    End of Section 16

--- generateSecret.sh
+++ generateSecret.sh
@@ -1,2 +1,5 @@
 dotnet user-secrets init -p API
 dotnet user-secrets set "TokenKey" "super secret key" -p API
+dotnet user-secrets set "Cloudinary:CloudName" "nxyang" -p API
+dotnet user-secrets set "Cloudinary:ApiKey" "254831276732133" -p API
+dotnet user-secrets set "Cloudinary:ApiSecret" "Zbycs1GXM5CEF6OGr5icMRoR-fg" -p API
