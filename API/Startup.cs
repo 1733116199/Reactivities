b03d7c8f8d05ffd6f9d4232657e88ad9ee93e4a2
Ningfeng <1733116199@qq.com>
2020-04-29 00:07:16 -0700

    End of Section 22

--- API/Startup.cs
+++ API/Startup.cs
@@ -156,6 +156,25 @@ namespace API
                 // app.UseDeveloperExceptionPage();
 
             }
+            // app.UseHsts();
+
+            app.UseXContentTypeOptions();
+
+            app.UseReferrerPolicy(opt => opt.NoReferrer());
+
+            app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
+
+            app.UseXfo(opt => opt.Deny());
+
+            app.UseCsp(
+                opt => opt.BlockAllMixedContent()
+                .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"))
+                .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
+                .FormActions(s => s.Self())
+                .FrameAncestors(s => s.Self())
+                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com", "blob:", "data:"))
+                .ScriptSources(s => s.Self().CustomSources("sha256-ma5XxS1EBgt17N22Qq31rOxxRWRfzUTQS1KOtfYwuNo="))
+            );
 
             // app.UseHttpsRedirection();
             app.UseDefaultFiles();
